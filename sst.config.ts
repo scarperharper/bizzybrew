1d4ab2a5d371c22576b2fe6441866f351f6d08fa
Simon Harper <simon.d.harper@gmail.com>
2026-01-04 21:04:58 +0000

    github

--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,26 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "bizzybrew",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
-    };
-  },
-  async run() {
-    new sst.aws.React("MyWeb");
-  },
+	app(input) {
+		return {
+			name: 'bizzybrew',
+			removal: input?.stage === 'production' ? 'retain' : 'remove',
+			protect: ['production'].includes(input?.stage),
+			home: 'aws',
+		};
+	},
+	async run() {
+		const DATABASE_URL = new sst.Secret('DATABASE_URL');
+		const SUPABASE_ANON_KEY = new sst.Secret('SUPABASE_ANON_KEY');
+		const SUPABASE_URL = new sst.Secret('SUPABASE_URL');
+		new sst.aws.React('bizzybrew', {
+			link: [SUPABASE_ANON_KEY, SUPABASE_URL, DATABASE_URL],
+			domain: 'bizzybrew.com',
+			environment: {
+				DATABASE_URL: DATABASE_URL.value,
+				SUPABASE_ANON_KEY: SUPABASE_ANON_KEY.value,
+				SUPABASE_URL: SUPABASE_URL.value,
+			},
+		});
+	},
 });
