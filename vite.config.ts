0f7a0499a231b4f374e72e5c7ebebba1f00191a4
Simon Harper <simon.d.harper@gmail.com>
2026-06-28 21:09:21 +0100

    chore: upgrades

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [reactRouter(), tsconfigPaths()],
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
